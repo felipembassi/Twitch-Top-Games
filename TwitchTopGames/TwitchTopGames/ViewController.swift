02706b74fe298219b572d82ddf3f0e166f6e6d39
Felipe Bassi <felipe.mbassi@slserv.com.br>
2019-09-02 11:16:23 -0300

    Inicial commit

---
+++ TwitchTopGames/TwitchTopGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitchTopGames
+//
+//  Created by mazza on 02/09/19.
+//  Copyright © 2019 fbassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
